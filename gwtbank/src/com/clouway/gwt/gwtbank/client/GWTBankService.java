c9cbde89d00beb03f155b73e3831c3f39f11254e
IvanLazov <darkpain1989@gmail.com>
2012-05-23 18:24:31 +0300

    Added method for registering user

--- gwtbank/src/com/clouway/gwt/gwtbank/client/GWTBankService.java
+++ gwtbank/src/com/clouway/gwt/gwtbank/client/GWTBankService.java
@@ -1,8 +1,10 @@
 package com.clouway.gwt.gwtbank.client;
 
+import com.clouway.gwt.gwtbank.shared.User;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("GWTBankService")
 public interface GWTBankService extends RemoteService {
+  void register(User user);
 }
--- gwtbank/src/com/clouway/gwt/gwtbank/client/GWTBankServiceAsync.java
+++ gwtbank/src/com/clouway/gwt/gwtbank/client/GWTBankServiceAsync.java
@@ -1,7 +1,9 @@
 package com.clouway.gwt.gwtbank.client;
 
+import com.clouway.gwt.gwtbank.shared.User;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface GWTBankServiceAsync {
 
+  void register(User user, AsyncCallback<Void> callback);
 }
