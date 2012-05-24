4935f6d8deaf88536bd33a6b2786bd4546e93177
IvanLazov <darkpain1989@gmail.com>
2012-05-24 18:38:17 +0300

    Added BankServiceImpl

--- bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
@@ -1,8 +1,12 @@
 package com.clouway.gwt.bank.server;
 
+import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.clouway.gwt.bank.client.BankService;
 
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
 
+  public User registerUser(User user) {
+    return null;
+  }
 }
