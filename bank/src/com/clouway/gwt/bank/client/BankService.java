6722c4aed2706030295b902cca452351ce9cfcbf
IvanLazov <darkpain1989@gmail.com>
2012-05-24 18:38:04 +0300

    Added BakService & BankServiceAsync

--- bank/src/com/clouway/gwt/bank/client/BankService.java
+++ bank/src/com/clouway/gwt/bank/client/BankService.java
@@ -1,9 +1,10 @@
 package com.clouway.gwt.bank.client;
 
+import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("BankService")
 public interface BankService extends RemoteService {
-
+  User registerUser(User user);
 }
--- bank/src/com/clouway/gwt/bank/client/BankServiceAsync.java
+++ bank/src/com/clouway/gwt/bank/client/BankServiceAsync.java
@@ -1,4 +1,8 @@
 package com.clouway.gwt.bank.client;
 
+import com.clouway.gwt.bank.shared.User;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
 public interface BankServiceAsync {
+  void registerUser(User user, AsyncCallback<User> callback);
 }
