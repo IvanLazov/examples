1aa950d68827b9ac1bd0a32f29f0b2ead624f8d5
IvanLazov <darkpain1989@gmail.com>
2012-05-24 17:11:40 +0300

    Deleted files

--- gwtbank/src/com/clouway/gwt/gwtbank/client/GWTBankServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.clouway.gwt.gwtbank.client;
-
-import com.clouway.gwt.gwtbank.shared.User;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface GWTBankServiceAsync {
-
-  void register(User user, AsyncCallback<Void> callback);
-}
