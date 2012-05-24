9646ff83b3ad58323d6d08d24a6933e414371aff
IvanLazov <darkpain1989@gmail.com>
2012-05-24 18:36:42 +0300

    Added Presenter interface

---
+++ bank/src/com/clouway/gwt/bank/client/presenter/Presenter.java
@@ -0,0 +1,10 @@
+package com.clouway.gwt.bank.client.presenter;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public interface Presenter {
+  void go(final HasWidgets widgets);
+}
