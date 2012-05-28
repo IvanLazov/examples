06f02b33d668e602ca5ddefa1f956571e6184e74
IvanLazov <darkpain1989@gmail.com>
2012-05-28 19:45:55 +0300

    Added InvalidPasswordException.java

---
+++ bank/src/com/clouway/gwt/bank/client/register/exceptions/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package com.clouway.gwt.bank.client.register.exceptions;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class InvalidPasswordException extends RuntimeException {
+}
