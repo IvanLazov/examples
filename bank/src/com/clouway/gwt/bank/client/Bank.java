dc6a67f36cda9a1bbd697560350c299adef8044d
IvanLazov <darkpain1989@gmail.com>
2012-05-28 20:00:04 +0300

    * Create BankServiceAsync service, AppController in the onModuleLoad()

--- bank/src/com/clouway/gwt/bank/client/Bank.java
+++ bank/src/com/clouway/gwt/bank/client/Bank.java
@@ -1,10 +1,14 @@
 package com.clouway.gwt.bank.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Bank implements EntryPoint {
 
   public void onModuleLoad() {
-
+    BankServiceAsync rpcService = GWT.create(BankService.class);
+    AppController controller = new AppController(rpcService);
+    controller.go(RootPanel.get());
   }
 }
