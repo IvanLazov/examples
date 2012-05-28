89be442c587ca71ff45811adac1f13405fd15d17
IvanLazov <darkpain1989@gmail.com>
2012-05-28 20:03:06 +0300

    * Added InstanceMatcher class

---
+++ bank/test/com/clouway/gwt/bank/client/register/InstanceMatcher.java
@@ -0,0 +1,28 @@
+package com.clouway.gwt.bank.client.register;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class InstanceMatcher<T> extends BaseMatcher<T> {
+
+  private T instance;
+
+  public boolean matches(Object item) {
+    try {
+      instance = (T) item;
+      return true;
+    } catch (ClassCastException exception) {
+      return false;
+    }
+  }
+
+  public void describeTo(Description description) {
+  }
+
+  public T getInstance() {
+    return instance;
+  }
+}
