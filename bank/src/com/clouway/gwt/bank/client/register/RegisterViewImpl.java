a474749af98bcff4437b15849114f190eaabf5ef
IvanLazov <darkpain1989@gmail.com>
2012-05-28 19:57:29 +0300

    * Added Label widget
    * Method getUser() returns a new User object from the view's input fields
    
    * Method onRegisterButtonClicked() now catches exception thrown by the presenter's registeUser() method.
    
    * Method clearFields() clears the input fields - username and password

--- bank/src/com/clouway/gwt/bank/client/register/RegisterViewImpl.java
+++ bank/src/com/clouway/gwt/bank/client/register/RegisterViewImpl.java
@@ -1,5 +1,7 @@
 package com.clouway.gwt.bank.client.register;
 
+import com.clouway.gwt.bank.client.register.exceptions.InvalidPasswordException;
+import com.clouway.gwt.bank.client.register.exceptions.InvalidUsernameException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -8,6 +10,8 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -24,13 +28,24 @@ public class RegisterViewImpl extends Composite implements RegisterView {
   TextBox username;
 
   @UiField
-  TextBox password;
+  PasswordTextBox password;
 
   @UiField
   Button register;
 
+  @UiField
+  Label notification;
+
   public RegisterViewImpl() {
     initWidget(uiBinder.createAndBindUi(this));
+  }
+
+  public User getUser() {
+    return new User(username.getText(), password.getText());
+  }
+
+  public void setNotification(String message) {
+    this.notification.setText(message);
   }
 
   public void setPresenter(Presenter presenter) {
@@ -40,7 +55,21 @@ public class RegisterViewImpl extends Composite implements RegisterView {
   @UiHandler("register")
   void onRegisterButtonClicked(ClickEvent event) {
     if (presenter != null) {
-      presenter.onRegisterUser(new User(username.getText(), password.getText()));
+
+      try {
+        presenter.registerUser();
+      } catch (InvalidUsernameException exception) {
+        notification.setText("Invalid username!");
+      } catch (InvalidPasswordException exception) {
+        notification.setText("Invalid password!");
+      }
+
+      clearFields();
     }
+  }
+
+  public void clearFields() {
+    username.setText("");
+    password.setText("");
   }
 }
