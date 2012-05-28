00a1aca8d250ecdab1e3cec57b90486e24d69031
IvanLazov <darkpain1989@gmail.com>
2012-05-28 20:02:22 +0300

    * Added getUsername() and getPassword methods.

--- bank/src/com/clouway/gwt/bank/shared/User.java
+++ bank/src/com/clouway/gwt/bank/shared/User.java
@@ -1,17 +1,29 @@
 package com.clouway.gwt.bank.shared;
 
-import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.io.Serializable;
 
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class User implements IsSerializable {
+public class User implements Serializable {
 
   private String username;
   private String password;
 
+  public User() {
+  }
+
   public User(String username, String password) {
     this.username = username;
     this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
   }
 }
