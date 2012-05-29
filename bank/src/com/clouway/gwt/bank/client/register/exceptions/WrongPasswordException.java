d9332da15db278be1af517e32a73ecdd0d2d1cb5
IvanLazov <darkpain1989@gmail.com>
2012-05-29 15:45:32 +0300

    * WrongPasswordException now extends Exception and implements Serializable

--- bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongPasswordException.java
+++ bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongPasswordException.java
@@ -1,7 +1,9 @@
 package com.clouway.gwt.bank.client.register.exceptions;
 
+import java.io.Serializable;
+
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class WrongPasswordException extends RuntimeException {
+public class WrongPasswordException extends Exception implements Serializable {
 }
