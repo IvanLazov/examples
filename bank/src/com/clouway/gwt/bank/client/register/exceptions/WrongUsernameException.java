f910a754657d68cf52d2f482e4b9b453e04ea038
IvanLazov <darkpain1989@gmail.com>
2012-05-29 15:46:01 +0300

    * WrongUsernameException extends Exception implements Serializable

--- bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongUsernameException.java
+++ bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongUsernameException.java
@@ -1,7 +1,9 @@
 package com.clouway.gwt.bank.client.register.exceptions;
 
+import java.io.Serializable;
+
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class WrongUsernameException extends RuntimeException {
+public class WrongUsernameException extends Exception implements Serializable {
 }
