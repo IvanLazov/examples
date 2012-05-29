72487ffcfb0f4aea648f99480025d56957d31a08
IvanLazov <darkpain1989@gmail.com>
2012-05-29 09:48:28 +0300

    * WrongUsernameException

--- bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongUsernameException.java
+++ bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongUsernameException.java
@@ -3,5 +3,5 @@ package com.clouway.gwt.bank.client.register.exceptions;
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class InvalidUsernameException extends RuntimeException {
+public class WrongUsernameException extends RuntimeException {
 }
