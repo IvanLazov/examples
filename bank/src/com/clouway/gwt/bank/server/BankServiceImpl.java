8d35881d8818f687bb1e49d86d27eecaebcbc2d3
IvanLazov <darkpain1989@gmail.com>
2012-05-29 16:14:25 +0300

    * Method now throws WrongUsernameException and WrongPasswordException

--- bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
@@ -1,12 +1,14 @@
 package com.clouway.gwt.bank.server;
 
+import com.clouway.gwt.bank.client.register.exceptions.WrongPasswordException;
+import com.clouway.gwt.bank.client.register.exceptions.WrongUsernameException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.clouway.gwt.bank.client.BankService;
 
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
 
-  public void registerUser(User user) {
+  public void registerUser(User user) throws WrongUsernameException, WrongPasswordException {
 
   }
 }
