285204f5bac9a755a38f7576a9973305acc8e85a
IvanLazov <darkpain1989@gmail.com>
2012-05-29 09:48:15 +0300

    * Renamed to WrongPasswordException

--- bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongPasswordException.java
+++ bank/src/com/clouway/gwt/bank/client/register/exceptions/WrongPasswordException.java
@@ -3,5 +3,5 @@ package com.clouway.gwt.bank.client.register.exceptions;
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class InvalidPasswordException extends RuntimeException {
+public class WrongPasswordException extends RuntimeException {
 }
