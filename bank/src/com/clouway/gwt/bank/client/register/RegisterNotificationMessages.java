478235d70c15be706f0be6ff64fbfeadfb4d8dcb
IvanLazov <darkpain1989@gmail.com>
2012-05-30 15:22:05 +0300

    * Created RegisterNotificationMessages interface

---
+++ bank/src/com/clouway/gwt/bank/client/register/RegisterNotificationMessages.java
@@ -0,0 +1,11 @@
+package com.clouway.gwt.bank.client.register;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public interface RegisterNotificationMessages {
+
+  String wrongUsernameMessage();
+  String wrongPasswordMessage();
+  String successfulRegistrationMessage();
+}
