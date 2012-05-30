de1974fc2f2a98f4305e42f63e2ee4cfcad22d10
IvanLazov <darkpain1989@gmail.com>
2012-05-30 15:24:38 +0300

    * Added RegisterNotification as dependency in the constructor
    * Change the way notification messages are shown to the view, using the RegisterNotification

--- bank/src/com/clouway/gwt/bank/client/register/RegisterPresenter.java
+++ bank/src/com/clouway/gwt/bank/client/register/RegisterPresenter.java
@@ -2,8 +2,8 @@ package com.clouway.gwt.bank.client.register;
 
 import com.clouway.gwt.bank.client.BankServiceAsync;
 import com.clouway.gwt.bank.client.presenter.Presenter;
-import com.clouway.gwt.bank.client.register.exceptions.WrongPasswordException;
-import com.clouway.gwt.bank.client.register.exceptions.WrongUsernameException;
+import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
+import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -17,10 +17,12 @@ public class RegisterPresenter implements Presenter, RegisterView.Presenter {
 
   private final BankServiceAsync rpcService;
   private final RegisterView view;
+  private final RegisterNotificationMessages notification;
 
-  public RegisterPresenter(BankServiceAsync rpcService, RegisterView view) {
+  public RegisterPresenter(BankServiceAsync rpcService, RegisterView view, RegisterNotificationMessages notification) {
     this.rpcService = rpcService;
     this.view = view;
+    this.notification = notification;
   }
 
   public void go(HasWidgets container) {
@@ -33,12 +35,14 @@ public class RegisterPresenter implements Presenter, RegisterView.Presenter {
     User user = view.getUser();
 
     if (!matches(user.getUsername(),"^[a-zA-Z0-9]{1,20}$")) {
-      displayNotificationMessage("Username must contain only letters and digits! Length (1-20)");
+      view.setNotification(notification.wrongUsernameMessage());
+      view.clearPasswordField();
       return;
     }
 
     if (!matches(user.getPassword(),"^[a-zA-Z0-9]{6,20}$")) {
-      displayNotificationMessage("Password must contain only letters and digits! Length (6-20)");
+      view.setNotification(notification.wrongPasswordMessage());
+      view.clearPasswordField();
       return;
     }
 
@@ -46,26 +50,25 @@ public class RegisterPresenter implements Presenter, RegisterView.Presenter {
       public void onFailure(Throwable caught) {
 
         if (caught instanceof WrongUsernameException) {
-          displayNotificationMessage("Wrong username! Username must contain only letters and digits!");
+          view.setNotification(notification.wrongUsernameMessage());
+          view.clearPasswordField();
         }
 
         if (caught instanceof WrongPasswordException) {
-          displayNotificationMessage("Wrong password! Username must contain only letters and digits!");
+          view.setNotification(notification.wrongPasswordMessage());
+          view.clearPasswordField();
         }
       }
 
       public void onSuccess(Void result) {
-        displayNotificationMessage("Registration was successful!");
+        view.setNotification(notification.successfulRegistrationMessage());
+        view.clearUsernameField();
+        view.clearPasswordField();
       }
     });
   }
 
   private boolean matches(String input, String pattern) {
     return RegExp.compile(pattern).test(input);
-  }
-
-  private void displayNotificationMessage(String message) {
-    view.clearFields();
-    view.setNotification(message);
   }
 }
