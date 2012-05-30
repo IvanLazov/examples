8e7f6b9a174e16ff2defd0c3fcc08882e0f7a707
IvanLazov <darkpain1989@gmail.com>
2012-05-30 16:42:43 +0300

    * Change import package

--- bank/src/com/clouway/gwt/bank/client/BankService.java
+++ bank/src/com/clouway/gwt/bank/client/BankService.java
@@ -1,7 +1,7 @@
 package com.clouway.gwt.bank.client;
 
-import com.clouway.gwt.bank.client.register.exceptions.WrongPasswordException;
-import com.clouway.gwt.bank.client.register.exceptions.WrongUsernameException;
+import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
+import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
