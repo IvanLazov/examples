a9d7c424bc01caa5a9eb28045b011249c86e688e
IvanLazov <darkpain1989@gmail.com>
2012-05-31 19:45:02 +0300

    * Added EventBus instance and set as dependency to the AppController

--- bank/src/com/clouway/gwt/bank/client/Bank.java
+++ bank/src/com/clouway/gwt/bank/client/Bank.java
@@ -2,13 +2,16 @@ package com.clouway.gwt.bank.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Bank implements EntryPoint {
 
   public void onModuleLoad() {
     BankServiceAsync rpcService = GWT.create(BankService.class);
-    AppController controller = new AppController(rpcService);
+    EventBus eventBus = new SimpleEventBus();
+    AppController controller = new AppController(rpcService, eventBus);
     controller.go(RootPanel.get());
   }
 }
