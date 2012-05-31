f16f79571ff5a986e76e12d5df7d235f4049655f
IvanLazov <darkpain1989@gmail.com>
2012-05-31 19:46:16 +0300

    * Added empty implementation of loginUser(User user) method

--- bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
@@ -2,6 +2,7 @@ package com.clouway.gwt.bank.server;
 
 import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
 import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
+import com.clouway.gwt.bank.client.exceptions.WrongUsernameOrPasswordException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.clouway.gwt.bank.client.BankService;
@@ -9,5 +10,9 @@ import com.clouway.gwt.bank.client.BankService;
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
 
   public void registerUser(User user) throws WrongUsernameException, WrongPasswordException {
+  }
+
+  public User loginUser(User user) throws WrongUsernameOrPasswordException {
+    return null;
   }
 }
