f2ef829ed766c69fcc2e792c2edd8beac314ec18
IvanLazov <darkpain1989@gmail.com>
2012-05-31 19:45:28 +0300

    * Added method loginUser(User user)

--- bank/src/com/clouway/gwt/bank/client/BankService.java
+++ bank/src/com/clouway/gwt/bank/client/BankService.java
@@ -2,6 +2,7 @@ package com.clouway.gwt.bank.client;
 
 import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
 import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
+import com.clouway.gwt.bank.client.exceptions.WrongUsernameOrPasswordException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
@@ -9,4 +10,5 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("BankService")
 public interface BankService extends RemoteService {
   void registerUser(User user) throws WrongUsernameException, WrongPasswordException;
+  User loginUser(User user) throws WrongUsernameOrPasswordException;
 }
