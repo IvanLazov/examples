5907422a2b34c36d9feebcd01091f800bd1be888
IvanLazov <darkpain1989@gmail.com>
2012-06-01 15:10:42 +0300

    * Account class with methods for deposit and withdraw

---
+++ bank/src/com/clouway/gwt/bank/client/account/Account.java
@@ -0,0 +1,59 @@
+package com.clouway.gwt.bank.client.account;
+
+import com.clouway.gwt.bank.client.exceptions.DepositZeroAmountException;
+import com.clouway.gwt.bank.client.exceptions.ExcessDepositAmountException;
+import com.clouway.gwt.bank.client.exceptions.InsufficientFundsException;
+import com.clouway.gwt.bank.client.exceptions.NegativeDepositAmountException;
+import com.clouway.gwt.bank.client.exceptions.NegativeWithdrawAmountException;
+import com.clouway.gwt.bank.client.exceptions.WithdrawZeroAmountException;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class Account implements Serializable {
+
+  private double balance;
+
+  public Account(double amount) {
+    this.balance = amount;
+  }
+
+  public void deposit(double amount) {
+
+    if (amount > 10000.0) {
+      throw new ExcessDepositAmountException();
+    }
+
+    if (amount < 0) {
+      throw new NegativeDepositAmountException();
+    }
+
+    if (amount == 0) {
+      throw new DepositZeroAmountException();
+    }
+
+    this.balance += amount;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void withdraw(double amount) {
+
+    if (amount > balance) {
+      throw new InsufficientFundsException();
+    }
+
+    if (amount == 0) {
+      throw new WithdrawZeroAmountException();
+    }
+
+    if (amount < 0) {
+      throw new NegativeWithdrawAmountException();
+    }
+    this.balance -= amount;
+  }
+}
