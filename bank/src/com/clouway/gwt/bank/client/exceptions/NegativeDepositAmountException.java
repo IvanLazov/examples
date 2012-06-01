8f30f0b677447f625d5dfae722e4454bf8a19ad6
IvanLazov <darkpain1989@gmail.com>
2012-06-01 15:10:58 +0300

    * New Exception classes

---
+++ bank/src/com/clouway/gwt/bank/client/exceptions/NegativeDepositAmountException.java
@@ -0,0 +1,9 @@
+package com.clouway.gwt.bank.client.exceptions;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class NegativeDepositAmountException extends RuntimeException implements Serializable {
+}
