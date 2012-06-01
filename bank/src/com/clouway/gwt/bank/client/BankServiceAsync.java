6c0ca6d2619abcacc14037e5a8183d2ab963f86c
IvanLazov <darkpain1989@gmail.com>
2012-06-01 20:12:21 +0300

    * Added method for withdraw and deposit

--- bank/src/com/clouway/gwt/bank/client/BankServiceAsync.java
+++ bank/src/com/clouway/gwt/bank/client/BankServiceAsync.java
@@ -1,5 +1,6 @@
 package com.clouway.gwt.bank.client;
 
+import com.clouway.gwt.bank.client.exceptions.InsufficientFundsException;
 import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
@@ -7,4 +8,8 @@ public interface BankServiceAsync {
   void registerUser(User user, AsyncCallback<Void> callback);
 
   void loginUser(User user, AsyncCallback<User> callback);
+
+  void deposit(double amount, AsyncCallback<Double> async);
+
+  void withdraw(double amount, AsyncCallback<Double> async);
 }
