8719236d57595d8d56c328f6f74ff0dca0307ea2
IvanLazov <darkpain1989@gmail.com>
2012-06-01 20:12:45 +0300

    * Implementation of deposit and withdraw methods

--- bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
@@ -1,5 +1,7 @@
 package com.clouway.gwt.bank.server;
 
+import com.clouway.gwt.bank.client.account.Account;
+import com.clouway.gwt.bank.client.exceptions.InsufficientFundsException;
 import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
 import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
 import com.clouway.gwt.bank.client.exceptions.WrongUsernameOrPasswordException;
@@ -9,10 +11,22 @@ import com.clouway.gwt.bank.client.BankService;
 
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
 
+  private final Account account = new Account(0.0);
+
   public void registerUser(User user) throws WrongUsernameException, WrongPasswordException {
   }
 
   public User loginUser(User user) throws WrongUsernameOrPasswordException {
     return null;
+  }
+
+  public double deposit(double amount) {
+    account.deposit(amount);
+    return account.getBalance();
+  }
+
+  public double withdraw(double amount) throws InsufficientFundsException {
+    account.withdraw(amount);
+    return account.getBalance();
   }
 }
