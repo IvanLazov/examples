db3d659e4417b359c83b16f440cd32e39dbc18b1
IvanLazov <darkpain1989@gmail.com>
2012-06-04 19:39:40 +0300

    * Added UserServiceAsync as dependency

--- bank/src/com/clouway/gwt/bank/client/BankServiceAsync.java
+++ bank/src/com/clouway/gwt/bank/client/BankServiceAsync.java
@@ -1,13 +1,8 @@
 package com.clouway.gwt.bank.client;
 
-import com.clouway.gwt.bank.client.exceptions.InsufficientFundsException;
-import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface BankServiceAsync {
-  void registerUser(User user, AsyncCallback<Void> callback);
-
-  void loginUser(User user, AsyncCallback<User> callback);
 
   void deposit(double amount, AsyncCallback<Double> async);
 
