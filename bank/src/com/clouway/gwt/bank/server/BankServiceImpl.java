124bade912763bac1a66bd8b9f0dc8c08d5a0789
IvanLazov <darkpain1989@gmail.com>
2012-06-04 20:56:21 +0300

    * Added AccountRepository as dependency

--- bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
@@ -1,9 +1,16 @@
 package com.clouway.gwt.bank.server;
 
+import com.clouway.gwt.bank.client.account.AccountRepository;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.clouway.gwt.bank.client.BankService;
 
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
+
+  private final AccountRepository repository;
+
+  public BankServiceImpl(AccountRepository repository) {
+    this.repository = repository;
+  }
 
   public double deposit(double amount) {
     return 0;
