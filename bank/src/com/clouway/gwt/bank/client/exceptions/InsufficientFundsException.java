a94dc0aa9b0703e65a2e55de98e7128653357475
IvanLazov <darkpain1989@gmail.com>
2012-06-04 19:36:59 +0300

    * Class extends RuntimeException

--- bank/src/com/clouway/gwt/bank/client/exceptions/InsufficientFundsException.java
+++ bank/src/com/clouway/gwt/bank/client/exceptions/InsufficientFundsException.java
@@ -5,5 +5,5 @@ import java.io.Serializable;
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class InsufficientFundsException extends Exception implements Serializable {
+public class InsufficientFundsException extends RuntimeException implements Serializable {
 }
