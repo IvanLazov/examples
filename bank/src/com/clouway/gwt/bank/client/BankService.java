00dbeda7e5594058a03ed83a60b6ba6359c096e8
IvanLazov <darkpain1989@gmail.com>
2012-06-04 19:39:35 +0300

    * Removed method registerUser() and loginUser()

--- bank/src/com/clouway/gwt/bank/client/BankService.java
+++ bank/src/com/clouway/gwt/bank/client/BankService.java
@@ -1,18 +1,11 @@
 package com.clouway.gwt.bank.client;
 
-import com.clouway.gwt.bank.client.exceptions.InsufficientFundsException;
-import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
-import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
-import com.clouway.gwt.bank.client.exceptions.WrongUsernameOrPasswordException;
-import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("BankService")
 public interface BankService extends RemoteService {
-  void registerUser(User user) throws WrongUsernameException, WrongPasswordException;
-  User loginUser(User user) throws WrongUsernameOrPasswordException;
 
   double deposit(double amount);
-  double withdraw(double amount) throws InsufficientFundsException;
+  double withdraw(double amount);
 }
