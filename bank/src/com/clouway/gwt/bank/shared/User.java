74fc210008a7257e3f907f15bde8c9598cf1d2e2
IvanLazov <darkpain1989@gmail.com>
2012-06-04 19:48:28 +0300

    * Added userId field

--- bank/src/com/clouway/gwt/bank/shared/User.java
+++ bank/src/com/clouway/gwt/bank/shared/User.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
  */
 public class User implements Serializable {
 
+  private long userId;
   private String username;
   private String password;
 
@@ -19,11 +20,43 @@ public class User implements Serializable {
     this.password = password;
   }
 
+  public User(long userId, String username, String password) {
+    this.userId = userId;
+    this.username = username;
+    this.password = password;
+  }
+
   public String getUsername() {
     return username;
   }
 
   public String getPassword() {
     return password;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    User user = (User) o;
+
+    if (userId != user.userId) return false;
+    if (password != null ? !password.equals(user.password) : user.password != null) return false;
+    if (username != null ? !username.equals(user.username) : user.username != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = (int) (userId ^ (userId >>> 32));
+    result = 31 * result + (username != null ? username.hashCode() : 0);
+    result = 31 * result + (password != null ? password.hashCode() : 0);
+    return result;
   }
 }
