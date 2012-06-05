1ebc6eb0ab29933a2ca83239c731f57025a3dca2
IvanLazov <darkpain1989@gmail.com>
2012-06-05 10:27:18 +0300

    * Implementation of BankServiceImpl class

--- bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/BankServiceImpl.java
@@ -1,22 +1,32 @@
 package com.clouway.gwt.bank.server;
 
+import com.clouway.gwt.bank.client.account.Account;
 import com.clouway.gwt.bank.client.account.AccountRepository;
+import com.clouway.gwt.bank.shared.User;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.clouway.gwt.bank.client.BankService;
 
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
 
   private final AccountRepository repository;
+  private User user;
 
-  public BankServiceImpl(AccountRepository repository) {
+  public BankServiceImpl(AccountRepository repository, User user) {
     this.repository = repository;
+    this.user = user;
   }
 
   public double deposit(double amount) {
-    return 0;
+    Account account = repository.getAccount(user.getUserId());
+    account.deposit(amount);
+    repository.updateAccount(user.getUserId(), account.getBalance());
+    return account.getBalance();
   }
 
   public double withdraw(double amount) {
-    return 0;
+    Account account = repository.getAccount(user.getUserId());
+    account.withdraw(amount);
+    repository.updateAccount(user.getUserId(), account.getBalance());
+    return account.getBalance();
   }
 }
