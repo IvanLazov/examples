52e5320b89dd62f32e814e5b007194bd722bae6e
IvanLazov <darkpain1989@gmail.com>
2012-06-08 09:32:36 +0300

    * MainPageServlet

---
+++ bank/src/com/clouway/gwt/bank/server/MainPageServlet.java
@@ -0,0 +1,34 @@
+package com.clouway.gwt.bank.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class MainPageServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    PrintWriter writer = resp.getWriter();
+    resp.setContentType("text/html");
+    writer.println(
+            "<html>\n" +
+                    "<head>\n" +
+                    "    <title>Bank</title>\n" +
+                    "    <link type=\"text/css\" rel=\"stylesheet\" href=\"Bank.css\">\n" +
+                    "    <script type=\"text/javascript\" language=\"javascript\" src=\"Bank/Bank.nocache.js\"></script>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "\n" +
+                    "</body>\n" +
+                    "</html>\n"
+    );
+
+    writer.flush();
+  }
+}
