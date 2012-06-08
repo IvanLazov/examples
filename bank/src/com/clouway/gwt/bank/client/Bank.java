7b391d687ca838b3073a030aa17422ca577c3d27
IvanLazov <darkpain1989@gmail.com>
2012-06-08 09:31:40 +0300

    * Bank - entryPoint

--- bank/src/com/clouway/gwt/bank/client/Bank.java
+++ bank/src/com/clouway/gwt/bank/client/Bank.java
@@ -4,15 +4,44 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.rpc.XsrfToken;
+import com.google.gwt.user.client.rpc.XsrfTokenService;
+import com.google.gwt.user.client.rpc.XsrfTokenServiceAsync;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Bank implements EntryPoint {
 
   public void onModuleLoad() {
-    UserServiceAsync userRpcService = GWT.create(UserService.class);
-    BankServiceAsync bankRpcService = GWT.create(BankService.class);
-    EventBus eventBus = new SimpleEventBus();
-    AppController controller = new AppController(userRpcService, bankRpcService, eventBus);
-    controller.go(RootPanel.get());
+
+    XsrfTokenServiceAsync xsrf = (XsrfTokenServiceAsync)GWT.create(XsrfTokenService.class);
+    ((ServiceDefTarget)xsrf).setServiceEntryPoint(GWT.getModuleBaseURL() + "xsrf");
+    xsrf.getNewXsrfToken(new AsyncCallback<XsrfToken>() {
+      @Override
+      public void onFailure(Throwable caught) {
+
+      }
+
+      @Override
+      public void onSuccess(XsrfToken result) {
+
+
+
+        LoginServiceAsync loginRpcService = GWT.create(LoginService.class);
+        RegisterServiceAsync registerRpcService = GWT.create(RegisterService.class);
+        AccountServiceAsync bankRpcService = GWT.create(AccountService.class);
+        EventBus eventBus = new SimpleEventBus();
+
+        AppController controller = new AppController(registerRpcService, loginRpcService, bankRpcService, eventBus);
+        controller.go(RootPanel.get());
+      }
+    });
+
+
+
+
+
+
   }
 }
