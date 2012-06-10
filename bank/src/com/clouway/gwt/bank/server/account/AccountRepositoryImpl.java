1895223e8a417eb8a477142f387d7c39522abd5a
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:35:28 +0300

    * Implementation of createAccount() method

--- bank/src/com/clouway/gwt/bank/server/account/AccountRepositoryImpl.java
+++ bank/src/com/clouway/gwt/bank/server/account/AccountRepositoryImpl.java
@@ -1,26 +1,31 @@
-package com.clouway.gwt.bank.client.account;
+package com.clouway.gwt.bank.server.account;
 
-import com.clouway.gwt.bank.server.AccountResultSetMapper;
 import com.clouway.gwt.bank.server.DatabaseHelper;
-import com.clouway.gwt.bank.server.ResultSetMapper;
+import com.clouway.gwt.bank.server.ResultSetBuilder;
+import com.google.inject.Inject;
 
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class AccountRepositoryImpl implements AccountRepository {
 
-  private ResultSetMapper<Account> mapper = new AccountResultSetMapper();
   private final DatabaseHelper databaseHelper;
+  private ResultSetBuilder<Account> builder = new AccountResultSetBuilder();
 
+  @Inject
   public AccountRepositoryImpl(DatabaseHelper databaseHelper) {
     this.databaseHelper = databaseHelper;
   }
 
   public Account getAccount(long userId) {
-    return databaseHelper.executeQuery("SELECT * FROM account WHERE userId=?", mapper, userId);
+    return databaseHelper.executeQuery("SELECT * FROM account WHERE userId=?", builder, userId);
   }
 
   public void updateAccount(long userId, double amount) {
     databaseHelper.executeQuery("UPDATE account SET balance=? WHERE userId=?", amount, userId);
+  }
+
+  public void createAccount(long userId) {
+    databaseHelper.executeQuery("INSERT INTO account(userId,balance) VALUES(?,?)", userId, 0.0);
   }
 }
