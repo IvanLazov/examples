5582c3c1756eb500cc2a1aa0b456ccb925366e67
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:42:34 +0300

    * Added Exception classes

---
+++ bank/src/com/clouway/gwt/bank/shared/exceptions/ExceededDepositException.java
@@ -0,0 +1,9 @@
+package com.clouway.gwt.bank.shared.exceptions;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class ExceededDepositException extends RuntimeException implements Serializable {
+}
---
+++ bank/src/com/clouway/gwt/bank/shared/exceptions/InvalidWithdrawException.java
@@ -0,0 +1,9 @@
+package com.clouway.gwt.bank.shared.exceptions;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class InvalidWithdrawException extends RuntimeException implements Serializable {
+}
