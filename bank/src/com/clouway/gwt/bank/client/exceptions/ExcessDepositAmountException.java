17d5f766a38a96c0e290a560959ff44caea19b26
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:47:37 +0300

    * Deleted classes. Some of them, because of renaming

--- bank/src/com/clouway/gwt/bank/client/exceptions/ExcessDepositAmountException.java
+++
@@ -1,9 +0,0 @@
-package com.clouway.gwt.bank.client.exceptions;
-
-import java.io.Serializable;
-
-/**
- * @author Ivan Lazov <darkpain1989@gmail.com>
- */
-public class ExcessDepositAmountException extends RuntimeException implements Serializable {
-}
