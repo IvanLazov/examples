2827dcaa151ea1f40aa6c03f77a674fdcfa202de
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:23:09 +0300

    * Added handling of UsernameAlreadyTakenException

--- bank/src/com/clouway/gwt/bank/client/register/RegisterPresenter.java
+++ bank/src/com/clouway/gwt/bank/client/register/RegisterPresenter.java
@@ -1,10 +1,11 @@
 package com.clouway.gwt.bank.client.register;
 
-import com.clouway.gwt.bank.client.UserServiceAsync;
+import com.clouway.gwt.bank.client.RegisterServiceAsync;
 import com.clouway.gwt.bank.client.presenter.Presenter;
-import com.clouway.gwt.bank.client.exceptions.WrongPasswordException;
-import com.clouway.gwt.bank.client.exceptions.WrongUsernameException;
 import com.clouway.gwt.bank.shared.User;
+import com.clouway.gwt.bank.shared.exceptions.UsernameAlreadyTakenException;
+import com.clouway.gwt.bank.shared.exceptions.WrongPasswordException;
+import com.clouway.gwt.bank.shared.exceptions.WrongUsernameException;
 import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -15,10 +16,10 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class RegisterPresenter implements Presenter, RegisterView.Presenter {
 
-  private final UserServiceAsync rpcService;
+  private final RegisterServiceAsync rpcService;
   private final RegisterView view;
 
-  public RegisterPresenter(UserServiceAsync rpcService, RegisterView view) {
+  public RegisterPresenter(RegisterServiceAsync rpcService, RegisterView view) {
     this.rpcService = rpcService;
     this.view = view;
   }
@@ -32,37 +33,43 @@ public class RegisterPresenter implements Presenter, RegisterView.Presenter {
 
     User user = view.getUser();
 
-    if (!matches(user.getUsername(),"^[a-zA-Z0-9]{1,20}$")) {
-      view.showWrongUsernameNotification();
+    if (!matches(user.getUsername(), "^[a-zA-Z0-9]{1,20}$")) {
+      view.wrongUsernameNotification();
       view.clearPasswordField();
       return;
     }
 
-    if (!matches(user.getPassword(),"^[a-zA-Z0-9]{6,20}$")) {
-      view.showWrongPasswordNotification();
+    if (!matches(user.getPassword(), "^[a-zA-Z0-9]{6,20}$")) {
+      view.wrongPasswordNotification();
       view.clearPasswordField();
       return;
     }
 
     rpcService.registerUser(user, new AsyncCallback<Void>() {
       public void onFailure(Throwable caught) {
-
         if (caught instanceof WrongUsernameException) {
-          view.showWrongUsernameNotification();
+          view.wrongUsernameNotification();
           view.clearPasswordField();
         }
 
         if (caught instanceof WrongPasswordException) {
-          view.showWrongPasswordNotification();
+          view.wrongPasswordNotification();
+          view.clearPasswordField();
+        }
+
+        if (caught instanceof UsernameAlreadyTakenException) {
+          view.usernameAlreadyTakenNotification();
+          view.clearUsernameField();
           view.clearPasswordField();
         }
       }
 
       public void onSuccess(Void result) {
-        view.showSuccessfulRegistrationNotification();
+        view.registrationSuccessfulNotification();
         view.clearUsernameField();
         view.clearPasswordField();
       }
+
     });
   }
 
