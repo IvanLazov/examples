e38e783baea269b0d7fb1cbfda8b2bd8c3883de1
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:44:53 +0300

    * SessionRepositoryImplTest

---
+++ bank/test/com/clouway/gwt/bank/server/session/SessionRepositoryImplTest.java
@@ -0,0 +1,46 @@
+package com.clouway.gwt.bank.server.session;
+
+import com.clouway.gwt.bank.server.DatabaseHelper;
+import com.clouway.gwt.bank.shared.AuthorizedUser;
+import com.clouway.gwt.bank.shared.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class SessionRepositoryImplTest {
+
+  private DatabaseHelper databaseHelper = new DatabaseHelper();
+  private final User user = new User(1, "Test", "password");
+
+  private SessionRepositoryImpl repository;
+
+  @Before
+  public void setUp() {
+    databaseHelper.executeQuery("DELETE FROM session");
+    databaseHelper.executeQuery("DELETE FROM account");
+    databaseHelper.executeQuery("DELETE FROM user");
+
+    databaseHelper.executeQuery("INSERT INTO user(userId,username,password) VALUES(?,?,?)", user.getUserId(), user.getUsername(), user.getPassword());
+
+    repository = new SessionRepositoryImpl(databaseHelper);
+  }
+
+  @Test
+  public void happyPath() {
+
+    String tokenValue = "abcd1234";
+
+    repository.createSession(tokenValue, user.getUserId(), user.getUsername());
+    AuthorizedUser authorizedUser = repository.getUser(tokenValue);
+
+    assertThat(authorizedUser.getTokenValue(), is(equalTo(tokenValue)));
+    assertThat(authorizedUser.getUserId(), is(equalTo(user.getUserId())));
+    assertThat(authorizedUser.getUsername(), is(equalTo(user.getUsername())));
+  }
+}
