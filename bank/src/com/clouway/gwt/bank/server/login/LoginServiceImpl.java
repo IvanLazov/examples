b447b1874e4f552e54720d517e33279c70605aa5
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:37:44 +0300

    * Changes in loginUser() method

--- bank/src/com/clouway/gwt/bank/server/login/LoginServiceImpl.java
+++ bank/src/com/clouway/gwt/bank/server/login/LoginServiceImpl.java
@@ -1,29 +1,34 @@
 package com.clouway.gwt.bank.server.login;
 
 import com.clouway.gwt.bank.client.LoginService;
+import com.clouway.gwt.bank.server.session.SessionRepository;
+import com.clouway.gwt.bank.server.user.UserRepository;
 import com.clouway.gwt.bank.shared.AuthorizedUser;
-import com.clouway.gwt.bank.server.authorization.AuthorizationRepository;
 import com.clouway.gwt.bank.shared.User;
 import com.clouway.gwt.bank.shared.exceptions.WrongUsernameOrPasswordException;
 import com.google.gwt.user.client.rpc.RpcToken;
 import com.google.gwt.user.client.rpc.RpcTokenException;
 import com.google.gwt.user.client.rpc.XsrfToken;
 import com.google.gwt.user.server.rpc.XsrfProtectedServiceServlet;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
 import java.lang.reflect.Method;
 
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
+@Singleton
 public class LoginServiceImpl extends XsrfProtectedServiceServlet implements LoginService {
 
-  private final LoginRepository loginRepository;
-  private final AuthorizationRepository authorizationRepository;
+  private final UserRepository userRepository;
+  private final SessionRepository sessionRepository;
   private XsrfToken token;
 
-  public LoginServiceImpl(LoginRepository loginRepository, AuthorizationRepository authorizationRepository) {
-    this.loginRepository = loginRepository;
-    this.authorizationRepository = authorizationRepository;
+  @Inject
+  public LoginServiceImpl(UserRepository userRepository, SessionRepository sessionRepository) {
+    this.userRepository = userRepository;
+    this.sessionRepository = sessionRepository;
   }
 
   @Override
@@ -34,16 +39,14 @@ public class LoginServiceImpl extends XsrfProtectedServiceServlet implements Log
 
   public AuthorizedUser loginUser(User user) {
 
-    String tokenValue = token.getToken();
+    User returnedUser = userRepository.getUser(user.getUsername());
 
-    User actualUser = loginRepository.getUser(user);
-
-    if (actualUser == null) {
+    if (returnedUser == null) {
       throw new WrongUsernameOrPasswordException();
     }
 
-    AuthorizedUser authorizedUser = new AuthorizedUser(tokenValue, actualUser.getUserId(), actualUser.getUsername());
-    authorizationRepository.save(tokenValue, actualUser.getUserId(), actualUser.getUsername());
+    sessionRepository.createSession(token.getToken(), returnedUser.getUserId(), returnedUser.getUsername());
+    AuthorizedUser authorizedUser = sessionRepository.getUser(token.getToken());
 
     return authorizedUser;
   }
