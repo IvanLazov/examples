41ad8af25cd9a0bee0ee65b51663e0059aa64629
IvanLazov <darkpain1989@gmail.com>
2012-06-10 21:27:16 +0300

    * Added map holding Presenters, the AppController can work with.

--- bank/src/com/clouway/gwt/bank/client/AppController.java
+++ bank/src/com/clouway/gwt/bank/client/AppController.java
@@ -1,38 +1,23 @@
 package com.clouway.gwt.bank.client;
 
-import com.clouway.gwt.bank.client.account.AccountPresenter;
-import com.clouway.gwt.bank.client.account.AccountView;
-import com.clouway.gwt.bank.client.account.AccountViewImpl;
-import com.clouway.gwt.bank.client.login.LoginPresenter;
-import com.clouway.gwt.bank.client.login.LoginView;
-import com.clouway.gwt.bank.client.login.LoginViewImpl;
 import com.clouway.gwt.bank.client.presenter.Presenter;
-import com.clouway.gwt.bank.client.register.RegisterPresenter;
-import com.clouway.gwt.bank.client.register.RegisterView;
-import com.clouway.gwt.bank.client.register.RegisterViewImpl;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HasWidgets;
+
+import java.util.Map;
 
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class AppController implements Presenter, ValueChangeHandler<String> {
 
-  private final UserServiceAsync userRpcService;
-  private final BankServiceAsync bankRpcService;
   private HasWidgets container;
-  private RegisterView registerView;
-  private LoginView loginView;
-  private EventBus eventBus;
-  private AccountView accountView;
+  private final Map<String, Presenter> userPresenters;
 
-  public AppController(UserServiceAsync userRpcService, BankServiceAsync bankRpcService, EventBus eventBus) {
-    this.userRpcService = userRpcService;
-    this.bankRpcService = bankRpcService;
-    this.eventBus = eventBus;
+  public AppController(Map<String, Presenter> userPresenters) {
+    this.userPresenters = userPresenters;
     bind();
   }
 
@@ -56,28 +41,10 @@ public class AppController implements Presenter, ValueChangeHandler<String> {
     String token = event.getValue();
 
     if (token != null) {
-
-      if (token.equals("register")) {
-        if (registerView == null) {
-          registerView = new RegisterViewImpl();
-        }
-        new RegisterPresenter(userRpcService, registerView).go(container);
-      }
-
-      if (token.equals("login")) {
-
-        if (loginView == null) {
-          loginView = new LoginViewImpl();
-        }
-        new LoginPresenter(userRpcService, loginView).go(container);
-      }
-
-      if (token.equals("userpage")) {
-        if (accountView == null) {
-          accountView = new AccountViewImpl();
-        }
-        new AccountPresenter(bankRpcService, accountView).go(container);
+     if (userPresenters.get(token) != null) {
+       userPresenters.get(token).go(container);
       }
     }
   }
+
 }
