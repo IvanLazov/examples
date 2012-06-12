45e00a90835959996444e52700a6786122f97a25
IvanLazov <darkpain1989@gmail.com>
2012-06-12 16:35:11 +0300

    * Added getBalance() method

--- bank/src/com/clouway/gwt/bank/client/AccountService.java
+++ bank/src/com/clouway/gwt/bank/client/AccountService.java
@@ -2,15 +2,13 @@ package com.clouway.gwt.bank.client;
 
 import com.clouway.gwt.bank.shared.exceptions.ExceededDepositException;
 import com.clouway.gwt.bank.shared.exceptions.InsufficientFundsException;
-import com.clouway.gwt.bank.shared.exceptions.InvalidDepositException;
-import com.clouway.gwt.bank.shared.exceptions.InvalidWithdrawException;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 import com.google.gwt.user.client.rpc.XsrfProtectedService;
 
 @RemoteServiceRelativePath("accountService")
 public interface AccountService extends XsrfProtectedService {
 
-  double deposit(double amount) throws ExceededDepositException, InvalidDepositException;
-  double withdraw(double amount) throws InsufficientFundsException, InvalidWithdrawException;
-  void logoutUser();
+  double deposit(double amount) throws ExceededDepositException;
+  double withdraw(double amount) throws InsufficientFundsException;
+  double getBalance();
 }
