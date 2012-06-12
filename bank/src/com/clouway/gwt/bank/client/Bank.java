c64df79cadb207e7b67484711aa60777dd3ae312
IvanLazov <darkpain1989@gmail.com>
2012-06-12 16:39:15 +0300

    * Removed the use of PageService, and added new service - SessionService

--- bank/src/com/clouway/gwt/bank/client/Bank.java
+++ bank/src/com/clouway/gwt/bank/client/Bank.java
@@ -1,10 +1,5 @@
 package com.clouway.gwt.bank.client;
 
-import com.clouway.gwt.bank.client.login.LoginPresenter;
-import com.clouway.gwt.bank.client.login.LoginViewImpl;
-import com.clouway.gwt.bank.client.presenter.Presenter;
-import com.clouway.gwt.bank.client.register.RegisterPresenter;
-import com.clouway.gwt.bank.client.register.RegisterViewImpl;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -15,15 +10,9 @@ import com.google.gwt.user.client.rpc.XsrfTokenService;
 import com.google.gwt.user.client.rpc.XsrfTokenServiceAsync;
 import com.google.gwt.user.client.ui.RootPanel;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Bank implements EntryPoint {
 
   public void onModuleLoad() {
-
-    final Map<String, Presenter> userPresentersMap = new HashMap<String, Presenter>();
 
     XsrfTokenServiceAsync xsrf = (XsrfTokenServiceAsync) GWT.create(XsrfTokenService.class);
     ((ServiceDefTarget) xsrf).setServiceEntryPoint(GWT.getModuleBaseURL() + "xsrf");
@@ -42,27 +31,11 @@ public class Bank implements EntryPoint {
         AccountServiceAsync accountRpcService = GWT.create(AccountService.class);
         ((HasRpcToken) accountRpcService).setRpcToken(token);
 
-        final Map<String, Presenter> presentersMap = new HashMap<String, Presenter>();
-        presentersMap.put("register", new RegisterPresenter(registerRpcService, new RegisterViewImpl()));
-        presentersMap.put("login", new LoginPresenter(userPresentersMap, accountRpcService, loginRpcService, new LoginViewImpl()));
+        SessionServiceAsync sessionRpcService = GWT.create(SessionService.class);
+        ((HasRpcToken) sessionRpcService).setRpcToken(token);
 
-        PageServiceAsync pageRpcService = GWT.create(PageService.class);
-        ((HasRpcToken) pageRpcService).setRpcToken(token);
-        pageRpcService.getPages(new AsyncCallback<List<String>>() {
-          public void onFailure(Throwable caught) {
-          }
-
-          public void onSuccess(List<String> pages) {
-            for (String page : pages) {
-              if (presentersMap.containsKey(page)) {
-                userPresentersMap.put(page, presentersMap.get(page));
-              }
-            }
-
-            AppController controller = new AppController(userPresentersMap);
-            controller.go(RootPanel.get());
-          }
-        });
+        AppController controller = new AppController(loginRpcService, registerRpcService, accountRpcService, sessionRpcService);
+        controller.go(RootPanel.get());
       }
     });
   }
