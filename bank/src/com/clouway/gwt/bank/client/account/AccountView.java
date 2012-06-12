3a6467f8380e98d0e0304e60ca7e6c207d74ccbe
IvanLazov <darkpain1989@gmail.com>
2012-06-12 16:31:19 +0300

    * Removed unnecessary methods

--- bank/src/com/clouway/gwt/bank/client/account/AccountView.java
+++ bank/src/com/clouway/gwt/bank/client/account/AccountView.java
@@ -13,16 +13,10 @@ public interface AccountView {
 
   void setPresenter(Presenter presenter);
   void clearInputField();
-  String getEnteredAmount();
-
   void updatedBalanceNotification(String amount);
   void successfulDepositNotification();
-  void zeroDepositNotification();
-  void exceededDepositNotification();
-  void negativeDepositNotification();
-  void incorrectInputNotification();
   void successfulWithdrawNotification();
-  void zeroWithdrawNotification();
-  void insufficientFundsNotification();
-  void negativeWithdrawNotification();
+  void invalidDepositNotification();
+  void invalidWithdrawNotification();
+  String getEnteredAmount();
 }
