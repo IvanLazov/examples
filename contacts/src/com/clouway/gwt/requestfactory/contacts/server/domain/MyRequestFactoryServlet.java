79fc2ff648d12addf6a83cafc05b62e87bbbd7ba
IvanLazov <darkpain1989@gmail.com>
2012-06-18 13:42:47 +0300

    * MyRequestFactoryServlet

---
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/server/domain/MyRequestFactoryServlet.java
@@ -0,0 +1,14 @@
+package com.clouway.gwt.requestfactory.contacts.server.domain;
+
+import com.google.web.bindery.requestfactory.server.RequestFactoryServlet;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class MyRequestFactoryServlet extends RequestFactoryServlet {
+
+  //@Inject
+  //public MyRequestFactoryServlet(MyDecorator decorator) {
+    //super(new DefaultExceptionHandler(), decorator);
+  //}
+}
