921c1cf3f04ecae8e862b8a9deeea8a68b5313df
IvanLazov <darkpain1989@gmail.com>
2012-06-18 15:37:27 +0300

    * Implementation of PersonServiceImpl class

---
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/server/domain/PersonServiceImpl.java
@@ -0,0 +1,24 @@
+package com.clouway.gwt.requestfactory.contacts.server.domain;
+
+import com.google.inject.Inject;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class PersonServiceImpl implements PersonService {
+
+  private PersonRepository repository;
+
+  @Inject
+  public PersonServiceImpl(PersonRepository repository) {
+    this.repository = repository;
+  }
+
+  public void save(Person person) {
+    repository.save(person);
+  }
+
+  public Person findPerson(Long id) {
+    return repository.findPerson(id);
+  }
+}
