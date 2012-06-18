f12f18575c9c85c49301eeda67ec8c9cc662c3f0
IvanLazov <darkpain1989@gmail.com>
2012-06-18 16:58:11 +0300

    * Removed package

--- contacts/src/com/clouway/gwt/requestfactory/contacts/client/AddContactViewImpl.java
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/client/AddContactViewImpl.java
@@ -1,7 +1,6 @@
 package com.clouway.gwt.requestfactory.contacts.client;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
