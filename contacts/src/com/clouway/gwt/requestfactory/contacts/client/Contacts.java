d99964a239ad2a69f67956e95fb3f548824a2958
IvanLazov <darkpain1989@gmail.com>
2012-06-18 16:58:41 +0300

    * Initialize RequestFactory with EventBus

--- contacts/src/com/clouway/gwt/requestfactory/contacts/client/Contacts.java
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/client/Contacts.java
@@ -4,12 +4,17 @@ import com.clouway.gwt.requestfactory.contacts.shared.ContactsRequestFactory;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
 public class Contacts implements EntryPoint {
 
   public void onModuleLoad() {
 
+    EventBus eventBus = new SimpleEventBus();
+
     final ContactsRequestFactory requestFactory = GWT.create(ContactsRequestFactory.class);
+    requestFactory.initialize(eventBus);
 
     AddContactViewImpl addContactView = new AddContactViewImpl();
     AddContactPresenter addContactPresenter = new AddContactPresenter(requestFactory, addContactView);
