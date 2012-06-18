fa6d329b3408724c9c82a920a242869e6c43b3c3
IvanLazov <darkpain1989@gmail.com>
2012-06-18 15:38:50 +0300

    * MyRequestFactoryServlet extends RequestFactoryServlet

--- contacts/src/com/clouway/gwt/requestfactory/contacts/server/domain/MyRequestFactoryServlet.java
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/server/domain/MyRequestFactoryServlet.java
@@ -1,14 +1,18 @@
 package com.clouway.gwt.requestfactory.contacts.server.domain;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.web.bindery.requestfactory.server.DefaultExceptionHandler;
 import com.google.web.bindery.requestfactory.server.RequestFactoryServlet;
 
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
+@Singleton
 public class MyRequestFactoryServlet extends RequestFactoryServlet {
 
-  //@Inject
-  //public MyRequestFactoryServlet(MyDecorator decorator) {
-    //super(new DefaultExceptionHandler(), decorator);
-  //}
+  @Inject
+  public MyRequestFactoryServlet(MyServiceLayerDecorator decorator) {
+    super(new DefaultExceptionHandler(), decorator);
+  }
 }
