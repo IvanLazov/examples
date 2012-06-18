3f042ad0141201fb02a470d8df1b5da621b77a4f
IvanLazov <darkpain1989@gmail.com>
2012-06-18 15:36:25 +0300

    * Added locator for PersonService

--- contacts/src/com/clouway/gwt/requestfactory/contacts/shared/PersonRequest.java
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/shared/PersonRequest.java
@@ -1,9 +1,7 @@
 package com.clouway.gwt.requestfactory.contacts.shared;
 
-import com.clouway.gwt.requestfactory.contacts.server.domain.Person;
-import com.clouway.gwt.requestfactory.contacts.server.domain.PersonLocator;
-import com.clouway.gwt.requestfactory.contacts.server.domain.PersonRepository;
-import com.clouway.gwt.requestfactory.contacts.server.domain.PersonRepositoryImpl;
+import com.clouway.gwt.requestfactory.contacts.server.domain.MyServiceLocator;
+import com.clouway.gwt.requestfactory.contacts.server.domain.PersonService;
 import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
 import com.google.web.bindery.requestfactory.shared.Service;
@@ -11,7 +9,7 @@ import com.google.web.bindery.requestfactory.shared.Service;
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-@Service(value = PersonRepository.class, locator = )
+@Service(value = PersonService.class, locator = MyServiceLocator.class)
 public interface PersonRequest extends RequestContext {
 
   Request<Void> save(PersonProxy personProxy);
