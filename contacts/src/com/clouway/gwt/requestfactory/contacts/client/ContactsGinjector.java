5cb91a044f311f70277022bac87c049b927ccdf2
IvanLazov <darkpain1989@gmail.com>
2012-06-19 15:06:27 +0300

    * ContactsGinjector have methods for injecting the ContactRequestFactory and AddContactView

---
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/client/ContactsGinjector.java
@@ -0,0 +1,15 @@
+package com.clouway.gwt.requestfactory.contacts.client;
+
+import com.clouway.gwt.requestfactory.contacts.shared.ContactsRequestFactory;
+import com.google.gwt.inject.client.GinModules;
+import com.google.gwt.inject.client.Ginjector;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+@GinModules(ContactsGinModule.class)
+public interface ContactsGinjector extends Ginjector {
+
+  ContactsRequestFactory injectContactsRequestFactory();
+  AddContactView injectAddContactView();
+}
