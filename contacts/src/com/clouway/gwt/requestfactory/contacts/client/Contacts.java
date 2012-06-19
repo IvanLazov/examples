8490b5fac9330a673fec091f4ae7a65b3b803123
IvanLazov <darkpain1989@gmail.com>
2012-06-19 15:05:20 +0300

    * Contacts class now use Gin DI to inject the ContactsRequestFactory and ContactAddView

--- contacts/src/com/clouway/gwt/requestfactory/contacts/client/Contacts.java
+++ contacts/src/com/clouway/gwt/requestfactory/contacts/client/Contacts.java
@@ -1,24 +1,16 @@
 package com.clouway.gwt.requestfactory.contacts.client;
 
-import com.clouway.gwt.requestfactory.contacts.shared.ContactsRequestFactory;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.web.bindery.event.shared.EventBus;
-import com.google.web.bindery.event.shared.SimpleEventBus;
 
 public class Contacts implements EntryPoint {
 
+  private final ContactsGinjector injector = GWT.create(ContactsGinjector.class);
+
   public void onModuleLoad() {
 
-    EventBus eventBus = new SimpleEventBus();
-
-    final ContactsRequestFactory requestFactory = GWT.create(ContactsRequestFactory.class);
-    requestFactory.initialize(eventBus);
-
-    AddContactViewImpl addContactView = new AddContactViewImpl();
-    AddContactPresenter addContactPresenter = new AddContactPresenter(requestFactory, addContactView);
-
+    AddContactPresenter addContactPresenter = new AddContactPresenter(injector.injectContactsRequestFactory(), injector.injectAddContactView());
     addContactPresenter.go(RootPanel.get());
   }
 }
